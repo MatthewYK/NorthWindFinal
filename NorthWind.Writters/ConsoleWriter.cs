736bbfe45c31976413699982c3129e08dc4b1789
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-17 15:19:47 -0500

    ConsoleWriter

---
+++ NorthWind.Writters/ConsoleWriter.cs
@@ -0,0 +1,21 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.Interfacess;
+using NorthWind.Entities.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Writters
+{
+    internal class ConsoleWriter : IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            Console.WriteLine("ConsoleWriter:{ 0},{ 1},{2}",
+                action.CreatedDateTime, action.User, action.Description;
+        }
+    }
+}
+
