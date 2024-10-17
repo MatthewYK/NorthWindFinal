fbd8042523d1f9b3cd2ad1d80d023b4537ebfea5
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-17 15:08:28 -0500

    IUserActionWriter

---
+++ NorthWind.Entities/Interfaces/IUserActionWriter.cs
@@ -0,0 +1,16 @@
+﻿using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Interfacess
+{
+    public interface IUserActionWriter
+    {
+        void Write(UserAction action);
+
+    }
+}
+
