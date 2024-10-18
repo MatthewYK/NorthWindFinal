3e2f33574ce4e60fc107cfb89d987772341034c1
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-17 22:01:59 -0500

    DebugWriter

---
+++ NorthWind.Writters/DebugWriter.cs
@@ -0,0 +1,20 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.Interfacess;
+using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Writters
+{
+    internal class DebugWriter : IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            DebugWriter.WriteLine("ConsoleWriter:{ 0},{ 1},{2}",
+                action.CreatedDateTime, action.User, action.Description);
+        }
+    }
+}
