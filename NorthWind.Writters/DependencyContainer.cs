344d10464dcf7402d552a8d1029d0f889ae5da54
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-17 22:31:29 -0500

    DependencyContainer

---
+++ NorthWind.Writters/DependencyContainer.cs
@@ -0,0 +1,32 @@
+﻿using NorthWind.Entities.Interfacess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Writters
+{
+    internal class DependencyContainer
+    {
+        public static IserviceCollection AddConsoleWriter(this IserviceCollection services)
+        {
+            service.AddSingleton<IUserActionWriter, ConsoleWriter>();
+            return services;
+        }
+        public static IserviceCollection AddDebugWriter(this IserviceCollection services)
+        {
+            service.AddSingleton<IUserActionWriter, DebugWriter>();
+            return services;
+        }
+        public static IserviceCollection AddFileWriter(this IserviceCollection services)
+        {
+            service.AddSingleton<IUserActionWriter, FileWriter>();
+            return services;
+        }
+    }
+
+    public class IserviceCollection
+    {
+    }
+}
