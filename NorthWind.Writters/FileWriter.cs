239a99bef39ed77d3ff669bec0b094fce2b0dfc0
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-17 22:28:00 -0500

    FileWriter

---
+++ NorthWind.Writters/FileWriter.cs
@@ -0,0 +1,21 @@
+﻿using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace NorthWind.Writters
+{
+    internal class FileWriter :Entities.Interfacess.IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            File.AppendAllText("AppLogs.txt",
+                string.Format("FileWriter: {0 },{1 },{2}",
+                action.CreatedDateTime, action.User, action.Description));
+                
+        }
+}
+}
